0c316d01da9212c2807d6be54cc5185c4ac7f07f
bprasad156 <156602266+bprasad156@users.noreply.github.com>
2024-05-18 15:43:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-const arr = [1, 2, 3, 4, 1, 0, 2, 2];
+///const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
+	const arr = [1, 2, 3, 4, 1, 0, 2, 2];
+	const result = [];
+    for (let i = 0; i < arr.length; i += size) {
+        result.push(arr.slice(i, i + size));
+    }
+    return result;
   // Write your code here
 };
+
+//const maxSum = 15;
 
 const n = prompt("Enter n: ");
 alert(JSON.stringify(divide(arr, n)));
